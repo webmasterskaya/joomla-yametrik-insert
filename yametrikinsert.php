32f6c3e218e2d795b7f7424aa47771a15a7eb49f
Artem Vasilev <kern.usr@gmail.com>
2020-05-16 16:10:22 +0300

    Копирайт

--- yametrikinsert.php
+++ yametrikinsert.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * @package    Joomla - Yandex.Metrika insert
+ * @version    __DEPLOY_VERSION__
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2018 - 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link       https://webmasterskaya.xyz/
+ */
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
