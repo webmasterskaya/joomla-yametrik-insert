3fec5ff5651d2797cb684436ea36a67419a04957
Artem Vasilev <kern.usr@gmail.com>
2020-05-17 13:45:55 +0300

    Добавил подключение стилей. Уменьшил размер окна custom-js

---
+++ fields/mediator.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package    Joomla - Yandex.Metrika insert
+ * @version    1.0.0-beta
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2018 - 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link       https://webmasterskaya.xyz/
+ */
+
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\HTML\HTMLHelper;
+
+defined('_JEXEC') or die;
+
+class JFormFieldMediator extends FormField
+{
+	protected $type = 'mediator';
+
+	protected function getInput()
+	{
+		if ((int) $this->element['styles'] == true)
+		{
+			HTMLHelper::_('stylesheet', 'plg_system_yametrikinsert/adminstyle.css', array('version' => 'auto', 'relative' => true));
+		}
+
+		if ((int) $this->element['script'] == true)
+		{
+			HTMLHelper::_('script', 'plg_system_yametrikinsert/adminscript.js', array('version' => 'auto', 'relative' => true));
+		}
+	}
+}
