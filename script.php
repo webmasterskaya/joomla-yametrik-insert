7f3053f91d79065bd60a7d8b06ed5b020ce25252
Artem Vasilev <kern.usr@gmail.com>
2020-05-17 13:00:54 +0300

    добавил install-скрипт и сообщение с приветсвием

---
+++ script.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    Joomla - Yandex.Metrika insert
+ * @version    1.0.0-beta
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2018 - 2020 Webmasterskaya. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link       https://webmasterskaya.xyz/
+ */
+
+defined('_JEXEC') or die;
+
+class plgSystemYaMetrikInsertInstallerScript
+{
+	function preflight($type, $parent)
+	{
+		jimport('joomla.version');
+		// Check compatible Joomla version
+		$jversion = new JVersion();
+
+		if (!$jversion->isCompatible('3.7'))
+		{
+			JFactory::getApplication()->enqueueMessage(JText::_('PLG_SYSTEM_YAMETRIKINSERT_WRONG_JOOMLA'), 'error');
+
+			return false;
+		}
+	}
+
+	function postflight($type, $parent)
+	{
+		JFactory::getApplication()->enqueueMessage(JText::_('PLG_SYSTEM_YAMETRIKINSERT_WELCOME_MESSAGE'), 'notice');
+	}
+}
